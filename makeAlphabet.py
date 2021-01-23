2789ed039c34f898900360b528029ea46be0a4bf
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-01-23 15:14:16 +0000

    add script to generate a monospace alphabet image

---
+++ makeAlphabet.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Import special modules ...
+try:
+    import PIL
+    import PIL.Image
+    import PIL.ImageDraw
+    import PIL.ImageFont
+except:
+    raise Exception("\"PIL\" is not installed; run \"pip install --user Pillow\"") from None
+
+# Import my modules ...
+try:
+    import pyguymer3
+except:
+    raise Exception("\"pyguymer3\" is not installed; you need to have the Python module from https://github.com/Guymer/PyGuymer3 located somewhere in your $PYTHONPATH") from None
+
+# Configure PIL to open images up to 1 GiP ...
+PIL.Image.MAX_IMAGE_PIXELS = 1024 * 1024 * 1024                                 # [px]
+
+# Define alphabet ...
+alphabet = "1234567890-: "
+
+# Define character spacing ...
+sp = 11                                                                         # [px]
+
+# Create empty white image and initialize the drawing and font objects ...
+im = PIL.Image.new("RGB", (sp * len(alphabet), 20), (255, 255, 255))
+dw = PIL.ImageDraw.Draw(im)
+ft = PIL.ImageFont.truetype("SFNSMono.ttf", 16)
+
+# Loop over characters in the alphabet ...
+for i, char in enumerate(alphabet):
+    # Draw character ...
+    dw.text((i * 11, 0), char, (0, 0, 0), font = ft)
+
+# Save image ...
+im.save("makeAlphabet.png")
+pyguymer3.optimize_image("makeAlphabet.png", strip = True)
